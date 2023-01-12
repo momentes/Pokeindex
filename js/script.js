6d02bd47867d9d9fe64fae10353bf5bf2c131347
Bryan S <72003881+momentes@users.noreply.github.com>
2023-01-12 13:48:13 +1100

    Test search with 2 api call

--- js/script.js
+++ js/script.js
@@ -50,22 +50,32 @@
 document.getElementById("searchBtn").addEventListener("click", function(){
     // get the search input
     const searchValue = document.getElementById("search").value;
-    // make an api call
+    // make an api call for pokemon details
     fetch(`https://pokeapi.co/api/v2/pokemon/${searchValue}`)
+    .then(response => response.json())
+    .then(data => {
+      let output = "";
+      output += `<img src='${data.sprites.front_default}' alt='${data.name}'/>`;
+      output += `<p>Name: ${data.name}</p>`;
+      output += `<p>Weight: ${data.weight}</p>`;
+      output += `<p>Height: ${data.height}</p>`;
+      output += `<p>Abilities: ${data.abilities.map(d=>d.ability.name).join(', ')}</p>`;
+      output += `<p>Type: ${data.types.map(d=>d.type.name).join(', ')}</p>`;
+      output += `<p>Stats: ${data.stats.map(d=>d.stat.name+ ' ' + d.base_stat).join(', ')}</p>`;
+      // make another api call for species details
+      fetch(`https://pokeapi.co/api/v2/pokemon-species/${searchValue}`)
       .then(response => response.json())
       .then(data => {
-        let output = "";
-        output += `<img src='${data.sprites.front_default}' alt='${data.name}'/>`;
-        output += `<p>Name: ${data.name}</p>`;
-        output += `<p>Weight: ${data.weight}</p>`;
-        output += `<p>Height: ${data.height}</p>`;
-        output += `<p>Abilities: ${data.abilities.map(d=>d.ability.name).join(', ')}</p>`;
-        output += `<p>Type: ${data.types.map(d=>d.type.name).join(', ')}</p>`;
-        output += `<p>Stats: ${data.stats.map(d=>d.stat.name+ ' ' + d.base_stat).join(', ')}</p>`;
+        let flavor_text = data.flavor_text_entries.filter(d=>d.language.name==='en');
+        output += `<p>Description: ${flavor_text[0].flavor_text}</p>`;
+        let form_description = data.varieties.filter(d=>d.is_default === true);
+        output += `<p>Form Description: ${form_description[0].form_description}</p>`;
+        let pokedex_number = data.pokedex_numbers.filter(d=>d.pokedex.name === "national");
+        output += `<p>Pokedex Number: ${pokedex_number[0].entry_number}</p>`;
         document.getElementById("output").innerHTML = output;
-      })
-      .catch(err => {
+        });
+    })
+    .catch(err => {
         document.getElementById("output").innerHTML = "Pokemon not found";
-      });
+    });
   });
-  
