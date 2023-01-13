0e1a8d08f4d3b58ea7283720f647f395ecadce6d
Bryan S <72003881+momentes@users.noreply.github.com>
2023-01-14 03:29:43 +1100

    Test branch

--- js/script.js
+++ js/script.js
@@ -91,31 +91,6 @@ var closeModal = document
 
 
 
-
-// document.getElementById("searchBtn").addEventListener("click", function(){
-//   const searchValue = document.getElementById("search").value;
-//   console.log(searchValue)
-//   fetch(`https://api.tcgdex.net/v2/en/cards/?name=${searchValue}`)
-//   .then(response => response.json())
-//   .then(data => {
-//     let output = "";
-//     const randomIndex = Math.floor(Math.random() * data.length); // generates a random number within the range of the number of results
-//     fetch(`https://api.tcgdex.net/v2/en/cards/${data[randomIndex].id}`)
-//         .then(response => response.json()).then((res) => {
-//         console.log(res)
-//         let output = "";
-//         output += `<img src='${data.image}/high.webp' alt='${data.name}'/>`;
-//         output += `<p>Name: <span>${data[randomIndex].name}</span></p>`;
-//         output += `<p>Attacks: ${data[randomIndex].attacks}</p>`;
-//         document.getElementById("modal-body").innerHTML = output;
-//         modal.style.display = "block";
-//     });
-//   });
-// });
-
-
-
-
 let searchSuccessful = false;
 document.getElementById("searchBtn").addEventListener("click", function(){
   const searchValue = document.getElementById("search").value;
@@ -131,7 +106,7 @@ document.getElementById("searchBtn").addEventListener("click", function(){
         let output = "";
         output += `<img src='${res.image}/high.webp' alt='${res.name}'/>`;
         output += `<p>Name: <span>${res.name}</span></p>`;
-        output += `<p>Attacks: ${res.attacks}</p>`;
+        output += `<p>Card Id: ${res.id}</p>`;
         document.getElementById("modal-body").innerHTML = output;
         modal.style.display = "block";
     });
@@ -141,92 +116,6 @@ document.getElementById("searchBtn").addEventListener("click", function(){
       document.getElementById("search").placeholder = "Pokemon not found";
   });
 });
-
-
-
-  // fetch(`https://api.tcgdex.net/v2/en/cards/?name=pikachu`)
-  // .then(response => response.json())
-  // .then(data => {
-  //   let output = "";
-  //   const randomIndex = Math.floor(Math.random() * data.length); // generates a random number within the range of the number of results
-  //   fetch(`https://api.tcgdex.net/v2/en/cards/${data[randomIndex].id}`)
-  //       .then(response => response.json()).then((res) => {
-  //       console.log(res)
-  //   });
-  // });
-
-
-
-
-//  document.getElementById("searchBtn").addEventListener("click", async function(){
-//   const searchValue = document.getElementById("search").value;
-//   const response = await fetch('https://api.pokemontcg.io/v2/cards?q=name:${searchValue}',{ 
-//     headers: new Headers({ 'X-Api-Key': '4d5790e7-adf3-4b11-b3e8-bc824e7e9445' })
-//   });
-//   let randomIndex = Math.floor(Math.random() * flavor_text_entries.length);
-//         output += `<p>Description: ${flavor_text_entries[randomIndex].flavor_text}</p>`;
-
-//   document.getElementById("modal-body").innerHTML = output;
-//     modal.style.display = "block";
-
-// });
-
-
-
-
-
-// document.getElementById("searchBtn").addEventListener("click", function(){
-//   const searchValue = document.getElementById("search").value;
-//   fetch(`https://api.tcgdex.net/v2/en/cards/${searchValue}`)
-//   .then(response => {
-//     if(response.ok){
-//         searchSuccessful = true;
-//         return response.json();
-//     } else {
-//         searchSuccessful = false;
-//         throw new Error("Pokemon not found");
-//     }
-//   })
-//   .then(data => {
-//     let output = "";
-//     output += `<img src='${data.imageUrlHiRes}' alt='${data.name}'/>`;
-//     output += `<p>Name: <span>${data.name}</span></p>`;
-//     output += `<p>Rarity: <span>${data.rarity}</span></p>`;
-//     document.getElementById("modal-body").innerHTML = output;
-//     document.getElementById("modal").style.display = "block";
-//   })
-//   .catch(err => {
-//     if(!searchSuccessful) {
-//       document.getElementById("search").value = "";
-//       document.getElementById("search").placeholder = "Pokemon not found";
-//     } else {
-//       console.log(err);
-//     }
-//   });
-// });
-
-
-
-
-
-// $("#searchBtn").on("click", function(event) {
-//   $("#card-container").empty();
-//   event.preventDefault();
-//   var pokemon = $("#search")
-//     .val()
-//     .trim();
-
-//     $.ajax({
-//       method: "GET",
-//       url: "https://api.pokemontcg.io/v1/cards?name=" + pokemon
-//     }).then(function(response) {
-//       var randomIndex = Math.floor(Math.random() * response.cards.length);
-//       var pokemonCard = $("<img class='card-display'>");
-//       pokemonCard.attr("src", response.cards[randomIndex].imageUrlHiRes);
-//       $("#card-container").append(pokemonCard);
-//     });
-// });
-
 
 
 
@@ -248,20 +137,6 @@ document.getElementById("searchBtn").addEventListener("click", function(){
 // });
 
 
-
-
-// function createCard(data) {
-//   //process the data to create a card element and return it
-//   //example:
-//   let card = $("<div>").addClass("card");
-//   let cardBody = $("<div>").addClass("card-body");
-//   let cardTitle = $("<h5>").addClass("card-title").text(data.name);
-//   let cardImage = $("<img>").addClass("card-img-top").attr("src", data.image);
-//   let cardText = $("<p>").addClass("card-text").text(data.description);
-//   cardBody.append(cardTitle, cardImage, cardText);
-//   card.append(cardBody);
-//   return card;
-// }
 
 
 
